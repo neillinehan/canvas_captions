f4cb204ed69402b4531763bf9d26c1d0184eec4e
root <root@418c67f1e790>
2024-06-11 18:15:24 +0000

    new repo

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+        name='canvas_captions',
+        version='0.1',
+        packages=find_packages(),
+        install_requires=[''],
+        author='neillinehan',
+        author_email='neiledwardlinehan@gmail.com',
+        description='Converts Canvas video JSON files from Wistia into text files.',
+        url='https://github.com/neillinehan/canvas_captions',
+)
